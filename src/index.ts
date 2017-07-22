732cf77dd5d212c9a353f47247142c631b0ecee4
hufeng <hufeng@qianmi.com>
2017-07-22 14:08:51 +0800

    modify typing path

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ import Store from './store';
 import StoreProvider from './store-provder';
 import { QL } from './ql';
 import Relax from './relax';
+import { IMap, IOptions } from './typing';
 
-export { QL, msg, Relax, Action, Actor, Store, StoreProvider };
+export { QL, msg, Relax, Action, Actor, Store, StoreProvider, IMap, IOptions };
