088f0abff3d6ce5854e443c1f623bb1dbc43360a
hufeng <hufeng@qianmi.com>
2017-08-15 13:57:56 +0800

    add jest --notify

--- src/msg.ts
+++ src/msg.ts
@@ -3,8 +3,12 @@
  * usage
  *  import {msg} from 'plume2'
  *
+ *  绑定
  *  msg.on('hello', (param) => console.log(param))
+ *  触发
  *  msg.emit('hello', 'hello')
+ *  解绑
+ *  msg.off('hello')
  */
 import * as mitt from 'mitt';
 
