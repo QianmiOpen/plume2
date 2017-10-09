130e69bad0ae057fe2dff5da39648889ef4b0ad7
hufeng <hufeng@qianmi.com>
2017-10-09 10:41:21 +0800

    release 0.2.2

--- packages/plume2/src/store-provder.tsx
+++ packages/plume2/src/store-provder.tsx
@@ -46,13 +46,14 @@ export default function StoreProvider(AppStore: TStore, opts?: IOptions) {
         if (process.env.NODE_ENV != 'production') {
           if ((this.store as any)._opts.debug) {
             if (window) {
-              var cssRule =
+              const cssRule =
                 'color: rgb(249, 162, 34);' +
                 'font-size: 40px;' +
                 'font-weight: bold;' +
                 'text-shadow: 1px 1px 5px rgb(249, 162, 34);' +
                 'filter: dropshadow(color=rgb(249, 162, 34), offx=1, offy=1);';
-              console.log('%cplume2🚀', cssRule);
+              const version = require('../package.json').version;
+              console.log(`%cplume2@${version}🚀`, cssRule);
             }
             console.log(`${WrapperComponent.displayName} will mount 🚀`);
           }
