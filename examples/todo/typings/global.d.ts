baa3d21082d5dcf1d4fdc5e2774c2dbb83bae253
hufeng <hufeng@qianmi.com>
2018-09-25 10:23:04 +0800

    fixed type

--- examples/todo/typings/global.d.ts
+++ examples/todo/typings/global.d.ts
@@ -1,8 +1,7 @@
-declare const __DEV__: boolean;
-
 import { TViewAction } from 'plume2';
 import * as viewAction from '../src/view-action';
 
 declare global {
+  declare const __DEV__: boolean;
   declare type TTodoViewAction = TViewAction<typeof viewAction>;
 }
