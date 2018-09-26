1f83e957698460a1840958866164dd39246a2842
hufeng <hufeng@qianmi.com>
2018-09-26 18:06:45 +0800

    add more babel optimize

--- packages/babel-plugin-plume2/src/__tests__/plugin.test.js
+++ packages/babel-plugin-plume2/src/__tests__/plugin.test.js
@@ -1,4 +1,4 @@
-const transform = require('../index.js');
+const plume2 = require('../index.js');
 const babel = require('babel-core');
 
 const example = `
@@ -6,16 +6,46 @@ import ReactDOM from 'react-dom';
 var ReactDOM1 = require('react-dom');`;
 
 describe('babel-plugin-plume2 test suite', () => {
-  it('origin code', () => {
+  it('no react-native code', () => {
     const { code } = babel.transform(example, {
-      plugins: [transform]
+      plugins: [plume2]
     });
     expect(code).toEqual(example);
   });
 
-  it('match-snapshot', () => {
+  it('react-native match-snapshot', () => {
     const { code } = babel.transform(example, {
-      plugins: [[transform, { reactnative: true }]]
+      plugins: [[plume2, { reactnative: true }]]
+    });
+    expect(code).toMatchSnapshot();
+  });
+
+  it('optimize QL name', () => {
+    const example = `
+    const helloQL = QL('helloQL', [/*...*/]);
+    const worldQL = QL('worldQL', [/*...*/]);
+    `;
+    const { code } = babel.transform(example, {
+      env: {
+        production: {
+          plugins: ['plume2']
+        }
+      }
+    });
+
+    expect(code).toMatchSnapshot();
+  });
+
+  it('optimize StoreProvider opts', () => {
+    const example = `
+    StoreProvider(AppStore, {debug: true})
+    `;
+    const { code } = babel.transform(example, {
+      env: {
+        production: {
+          plugins: ['plume2']
+        }
+      }
     });
     expect(code).toMatchSnapshot();
   });
--- packages/babel-plugin-plume2/src/index.js
+++ packages/babel-plugin-plume2/src/index.js
@@ -1,9 +1,13 @@
+const debug = require('debug')('babel-plugin-plume2');
+const isProd = (process.env.BABEL_ENV || process.env.NODE_ENV) === 'production';
+
+debug('production env? ', isProd);
+
 /**
  * 判断当前的call-expression是不是require('react-dom')
  * @param {*} t
  * @param {*} path
  */
-
 function isRequireReactDOM(t, path) {
   return (
     t.isIdentifier(path.node.callee, { name: 'require' }) &&
@@ -11,8 +15,12 @@ function isRequireReactDOM(t, path) {
   );
 }
 
-function isPlume2QL(path) {
+function isPlume2QL(t, path) {
   return t.isIdentifier(path.node.callee, { name: 'QL' });
+}
+
+function isStoreProvider(t, path) {
+  return t.isIdentifier(path.node.callee, { name: 'StoreProvider' });
 }
 
 module.exports = function(babel) {
@@ -38,14 +46,53 @@ module.exports = function(babel) {
       },
 
       /**
-       * if current env is react-native
-       * var ReactDOM = require('react-dom') => var ReactDOM = require('react-native');
        * @param {*} path
        * @param {*} param1
        */
       CallExpression(path, { opts }) {
+        /**
+         * if current env is react-native
+         * var ReactDOM = require('react-dom') => var ReactDOM = require('react-native');
+         */
         if (opts.reactnative && isRequireReactDOM(t, path)) {
+          debug(`react-native env replace react-dom -> react-native`);
           path.node.arguments[0] = t.StringLiteral('react-native');
+          return;
+        }
+
+        /**
+         * if current env is production
+         * transform
+         * const helloQL = QL('helloQL', [...])
+         * to =>
+         * const helloQL = QL('', [....])
+         * reduce javascript size
+         */
+        if (isProd && isPlume2QL(t, path)) {
+          const qlName = path.get('arguments.0').node.value;
+          if (qlName) {
+            debug(`optimize %s to empty str`, qlName);
+            //assign empty string
+            path.get('arguments.0').node.value = '';
+          }
+          return;
+        }
+
+        /**
+         * if current env is production
+         * transform
+         * StoreProvider(AppStore, {debug: true})
+         * to =>
+         * StoreProvider(AppStore)
+         */
+        if (isProd && isStoreProvider(t, path)) {
+          if (path.get('arguments') && path.get('arguments').length > 1) {
+            //get opts arguments
+            const opts = path.get('arguments.1');
+            opts && opts.remove();
+            debug(`optimize StoreProvider opts`);
+            return;
+          }
         }
       }
     }
